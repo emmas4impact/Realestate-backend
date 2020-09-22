d727ab6d05b213359bb11e1b554f6b904a10e332
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-09-22 11:18:49 +0300

    Create list.js

---
+++ src/Listings/list.js
@@ -0,0 +1,24 @@
+const express = require("express")
+
+const houseRoute =  express.Router()
+
+houseRoute.get("/", async(req, res, next)=>{
+    
+})
+
+houseRoute.get("/:id", async(req, res, next)=>{
+    
+})
+houseRoute.post("/", async(req, res, next)=>{
+    
+})
+
+houseRoute.put("/:id", async(req, res, next)=>{
+    
+})
+
+houseRoute.delete("/:id", async(req, res, next)=>{
+    
+})
+
+module.exports = houseRoute;
