1d895d11bfcb41c06d0a0713315b0ea62aeb9454
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-09-24 16:43:26 +0300

    forbiiden error handler

--- src/errorHandlers/index.js
+++ src/errorHandlers/index.js
@@ -4,7 +4,12 @@ const badRequestHandler = (err,req,res,next) =>{
     }
     next(err)
 }
-
+const forbiddenHandler = (err, req, res, next) => {
+    if (err.httpStatusCode === 403) {
+      res.status(403).send(err.message || "Forbidden!")
+    }
+    next(err)
+  } // 403
 const notFoundHandler = (err,req,res,next) => {
     if(err.httpStatusCode=== 404){
         res.status(404).send(err.message || "Resource not found!")
@@ -22,5 +27,6 @@ const genericErrorHandler = (err,req,res,next)=>{
 module.exports = {
     badRequestHandler,
     notFoundHandler,
+    forbiddenHandler,
     genericErrorHandler
 }
