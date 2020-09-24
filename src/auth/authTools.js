d7c9c41cc678ff1ec8b5bf7d843e92e2fad43fba
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-09-24 16:43:08 +0300

    error handlers added

--- src/auth/authTools.js
+++ src/auth/authTools.js
@@ -1,32 +1,96 @@
-const jwt =require("jsonwebtoken")
-const authenticate = async(user)=>{
-    try {
-        const newToken  = await generateJWT({_id: user._id})
-        return newToken
+const jwt = require("jsonwebtoken")
+const UserModel = require("../users/Schema")
 
-    } catch (error) {
-        throw new Error("Prolem with authentication")
-    }
+const authenticate = async (user) => {
+  try {
+    // generate tokens
+    const newAccessToken = await generateJWT({ _id: user._id })
+    console.log("new acces token ",newAccessToken)
+    const newRefreshToken = await generateRefreshJWT({ _id: user._id })
+
+    user.refreshTokens = user.refreshTokens.concat({ token: newRefreshToken })
+    await user.save()
+
+    return { token: newAccessToken, refreshToken: newRefreshToken }
+  } catch (error) {
+    console.log(error)
+    throw new Error(error)
+  }
 }
-const generateJWT = (payload) => new Promise((res, rej)=>{
-    jwt.sign(payload,
-     process.env.JWT_SECRET,
-    {expiresIn: "20m"},
-    (err, token)=>{
-        if(err)
-            rej(err)
+
+const generateJWT = (payload) =>
+  new Promise((res, rej) =>
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "6m"},
+      (err, token) => {
+        if (err) rej(err)
         res(token)
-    } )
-})
+      }
+    )
+  )
 
-const verifyJWT = (token) => new Promise((res, rej)=>{
-    jwt.verify(token,
-     process.env.JWT_SECRET,
-    (err, verified)=>{
-        if(err)
-            rej(err)
-        res(verified)
-    } )
-})
+const verifyJWT = (token) =>
+  new Promise((res, rej) =>
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) rej(err)
+      res(decoded)
+    })
+  )
 
-module.exports = {authenticate, verifyJWT}
+const generateRefreshJWT = (payload) =>
+  new Promise((res, rej) =>
+    jwt.sign(
+      payload,
+      "Hungry?Lion$",
+      { expiresIn: "1 week" },
+      (err, token) => {
+        if (err) rej(err)
+        res(token)
+      }
+    )
+  )
+
+const refreshToken = async (oldRefreshToken) => {
+  const decoded = await verifyRefreshToken(oldRefreshToken)
+
+  const user = await UserModel.findOne({ _id: decoded._id })
+
+  if (!user) {
+    throw new Error(`Access is forbidden`)
+  }
+
+  const currentRefreshToken = user.refreshTokens.find(
+    (t) => t.token === oldRefreshToken
+  )
+
+  if (!currentRefreshToken) {
+    throw new Error(`Refresh token is wrong`)
+  }
+
+  // generate tokens
+  const newAccessToken = await generateJWT({ _id: user._id })
+  const newRefreshToken = await generateRefreshJWT({ _id: user._id })
+
+  // save in db
+  const newRefreshTokens = user.refreshTokens
+    .filter((t) => t.token !== oldRefreshToken)
+    .concat({ token: newRefreshToken })
+
+  user.refreshTokens = [...newRefreshTokens]
+
+  await user.save()
+
+  return { token: newAccessToken, refreshToken: newRefreshToken }
+}
+
+const verifyRefreshToken = (token) =>
+  new Promise((res, rej) =>
+    jwt.verify(token, "Hungry?Lion$", (err, decoded) => {
+      if (err) rej(err)
+      res(decoded)
+    })
+  )
+
+module.exports = { authenticate, verifyJWT, refreshToken }
