459c247e8a8c6f9dd339bd7efac0db7c94f32704
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-09-29 16:39:34 +0300

    Update Schema.js

--- src/Tenants/Schema.js
+++ src/Tenants/Schema.js
@@ -1,53 +1,61 @@
-const { model, Schema } = require("mongoose");
+const {
+  model,
+  Schema
+} = require("mongoose");
 const valid = require("validator");
 
-const tenantSchema = new Schema(
-  {
-    name: { type: String, required: true },
+const tenantSchema = new Schema({
+    name: {
+      type: String,
+      required: true
+    },
     surname: {
       type: String,
       required: true,
     },
-    Employer:{
-        type: String,
-        required: true
-    },
-    
-    phone:{
+    Employer: {
       type: String,
-      required: true  
+      required: true
+    },
+
+    phone: {
+      type: String,
+      required: true
     },
     email: {
       type: String,
       required: true,
-      
+
     },
-    property:[{
-        type: Schema.Types.ObjectId, ref: 'house',
-       
-    }], //VALIDATION
-},
-  
-  { timestamps: true }
+    property: {
+      type: Schema.Types.ObjectId,
+      ref: 'house',
+
+    }, //VALIDATION
+  },
+
+  {
+    timestamps: true
+  }
 );
 
-ListingSchema.post("validate", function (error, doc, next) {
+tenantSchema.post("validate", function (error, doc, next) {
   if (error) {
-    error.httpStatusCode = 400 ;
-    next(error) ;
+    error.httpStatusCode = 400;
+    next(error);
   } else {
-    next() ;
+    next();
   }
-}) ;
+});
 
-ListingSchema.post("save", function (error, doc, next) {
+tenantSchema.post("save", function (error, doc, next) {
   if (error.name === "MongoError" && error.code === 11000) {
-    error.httpStatusCode = 400 ;
-    next(error) ;
+    error.httpStatusCode = 400;
+    next(error);
   } else {
-    next() ;
+    next();
   }
-}) ;
-const TenantModel = model("rent", tenantSchema );
+});
+const TenantModel = model("rent", tenantSchema);
 
-module.exports =TenantModel;
+module.exports = TenantModel;
