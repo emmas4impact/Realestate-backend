d03d0b28ef53c91eabd057140d0cb801f36475de
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-10-04 12:59:37 +0300

    Update Schema.js

--- src/Listings/Schema.js
+++ src/Listings/Schema.js
@@ -1,63 +1,91 @@
-const { model, Schema } = require("mongoose");
+const {
+  model,
+  Schema
+} = require("mongoose");
 const valid = require("validator");
-
-const ListingSchema = new Schema(
-  {
-    title: { type: String, required: true },
+const geocoder = require("../utils/geocoder")
+const ListingSchema = new Schema({
+    title: {
+      type: String,
+      required: true
+    },
     address: {
       type: String,
       required: true,
     },
-    region:{
+    region: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: {
         type: String,
-        required: true
+        enum: ["Point"],
+
+      },
+      coordinates: {
+        type: [Number],
+        index: "2dsphere",
+      },
+      formattedAddress: String,
     },
-    type:{
+    district: {
       type: String,
-      required: true  
-    },
-    district:{
-      type: String,
-      required: true  
+      required: true,
     },
     postcode: {
       type: String,
       required: true,
-      
     },
     price: {
       type: Number,
       required: true,
-    
     }, //VALIDATION
     rating: {
-        type: Number,
-        min: [1, "Sorry you can't rate below 1!"],
-        max: [5, "MAximum rating is 5"],
-        validate(value) {
-          
-            if (value < 0) {
-              throw new Error("rate must be a positive number!")
-            }
-          },
-          required: true,
+      type: Number,
+      min: [1, "Sorry you can't rate below 1!"],
+      max: [5, "MAximum rating is 5"],
+      validate(value) {
+        if (value < 0) {
+          throw new Error("rate must be a positive number!");
+        }
+      },
+      required: true,
     },
-    description: { type: String, required: true },
-    features: { type: Array, required: true },
-    details: { type: Array, required: true },
-    image: { type: Buffer, required: true, default: "https://img.fixthephoto.com/blog/images/gallery/news_image_212.jpg" },
+    description: {
+      type: String,
+      required: true
+    },
+    features: {
+      type: Array,
+      required: true
+    },
+    details: {
+      type: Array,
+      required: true
+    },
+    image: {
+      type: Buffer,
+      required: true,
+      default: "https://img.fixthephoto.com/blog/images/gallery/news_image_212.jpg",
+    },
     images: {
-        type: Array
+      type: Array,
     },
-    category:{
-        type: String,
-        required: true
+    category: {
+      type: String,
+      required: true,
     },
-   //validaton
-   
+    //validaton
   },
-  
-  { timestamps: true }
+
+  {
+    timestamps: true
+  }
 );
 // profileSchema.methods.toJSON = function () {
 //   const user = this
@@ -87,23 +115,28 @@ const ListingSchema = new Schema(
 //   }
 //   next() ;
 // }) ;
+
+ListingSchema.pre('save', async function (next) {
+  const loc = await geocoder.geocode(this.address)
+  console.log(loc)
+})
 ListingSchema.post("validate", function (error, doc, next) {
   if (error) {
-    error.httpStatusCode = 400 ;
-    next(error) ;
+    error.httpStatusCode = 400;
+    next(error);
   } else {
-    next() ;
+    next();
   }
-}) ;
+});
 
 ListingSchema.post("save", function (error, doc, next) {
   if (error.name === "MongoError" && error.code === 11000) {
-    error.httpStatusCode = 400 ;
-    next(error) ;
+    error.httpStatusCode = 400;
+    next(error);
   } else {
-    next() ;
+    next();
   }
-}) ;
+});
 const ListingModel = model("house", ListingSchema);
 
 module.exports = ListingModel;
