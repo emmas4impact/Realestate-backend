42cb271802456083d6dca778f4837d3638b11bfa
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-10-05 22:08:56 +0300

    sever js refractor

--- src/server.js
+++ src/server.js
@@ -1,50 +1,50 @@
-const express = require("express")
-const listEndpoints = require("express-list-endpoints")
-const mongoose = require("mongoose")
-const helmet = require("helmet")
-const cors = require("cors")
-const {join}= require("path")
-const cookieParser = require("cookie-parser")
-const passport = require("passport")
-const listingsRoute = require("./Listings/list")
-const tenantRoute = require("./Tenants/index")
-const userRoute = require("./users/index")
+const express = require("express");
+const listEndpoints = require("express-list-endpoints");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const cors = require("cors");
+const {
+    join
+} = require("path");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const listingsRoute = require("./Listings/list");
+const tenantRoute = require("./Tenants/index");
+const userRoute = require("./users/index");
 
 const {
     notFoundHandler,
     forbiddenHandler,
     badRequestHandler,
     genericErrorHandler,
-  } = require("./errorHandlers/index")
+} = require("./errorHandlers/index");
 const server = express();
-server.use(cors())
-server.use(cookieParser())
-server.use(helmet())
-const staticFolderPath = join(__dirname, "../public")
-server.use(express.static(staticFolderPath))
-console.log(staticFolderPath)
-const port = process.env.PORT || 2330 
-server.use(passport.initialize())
+server.use(cors());
+server.use(cookieParser());
+server.use(helmet());
+const staticFolderPath = join(__dirname, "../public");
+server.use(express.static(staticFolderPath));
+console.log(staticFolderPath);
 
-server.use(express.json())
-server.use("/listings", listingsRoute)
-server.use("/users", userRoute)
-server.use("/tenants", tenantRoute)
+server.use(passport.initialize());
 
-server.use(badRequestHandler)
-server.use(forbiddenHandler)
-server.use(notFoundHandler)
-server.use(genericErrorHandler)
+server.use(express.json());
+server.use("/listings", listingsRoute);
+server.use("/users", userRoute);
+server.use("/tenants", tenantRoute);
 
-console.log(listEndpoints(server))
+server.use(badRequestHandler);
+server.use(forbiddenHandler);
+server.use(notFoundHandler);
+server.use(genericErrorHandler);
+
+console.log(listEndpoints(server));
 mongoose
-.connect(process.env.MONGO_STRING,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    
-})
-.then(
-    server.listen(port,()=>{
-        console.log(`server is running on ${port}`)
+    .connect(process.env.MONGO_STRING, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
     })
-).catch((err)=> console.log(err))
+    .then(() => console.log("Mongo Db connected"))
+    .catch((err) => console.log(err));
+
+module.exports = server;
