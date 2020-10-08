519d07f2e630b2b0c977267368f60f3259eeaf83
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-10-08 03:15:59 +0300

    Update index.js

--- src/Tenants/index.js
+++ src/Tenants/index.js
@@ -1,31 +1,38 @@
-const express = require("express")
+const express = require("express");
 const TenantModel = require("./Schema");
-const ListingModel = require("../Listings/Schema")
-const userRoute = express.Router()
+const ListingModel = require("../Listings/Schema");
+const userRoute = express.Router();
 
 userRoute.get("/", async (req, res, next) => {
-    try {
-        const getTenant = await TenantModel.find(req.query).populate("house")
-        res.send({
-            data: getTenant,
-            Total: getTenant.length
-        })
-    } catch (error) {
-        next(error)
-    }
-})
-userRoute.get("/:id", async (req, res, next) => {})
+  try {
+    const getTenant = await TenantModel.find(req.query).populate("house");
+    res.send({
+      data: getTenant,
+      Total: getTenant.length,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+userRoute.get("/:id", async (req, res, next) => {
+  try {
+    const tenantById = await TenantModel.findById(req.params.id);
+    if (tenantById) res.status(200).send(tenantById);
+    else res.send(`No Tenant found with such Id: ${req.params.id}`);
+  } catch (error) {
+    next(error);
+  }
+});
 userRoute.post("/", async (req, res, next) => {
-    try {
-        const newTenant = new TenantModel(req.body)
-        await newTenant.save()
-        res.status(201).send(newTenant)
-    } catch (error) {
-        next(error)
-    }
-})
-userRoute.put("/:id", async (req, res, next) => {})
-userRoute.delete("/:id", async (req, res, next) => {})
-
+  try {
+    const newTenant = new TenantModel(req.body);
+    await newTenant.save();
+    res.status(201).send(newTenant);
+  } catch (error) {
+    next(error);
+  }
+});
+userRoute.put("/:id", async (req, res, next) => {});
+userRoute.delete("/:id", async (req, res, next) => {});
 
 module.exports = userRoute;
