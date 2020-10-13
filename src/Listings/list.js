f88d0a984d26d44c1188fbd22a68e485cc7ee6ca
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-10-13 18:48:26 +0300

    Update list.js

--- src/Listings/list.js
+++ src/Listings/list.js
@@ -1,24 +1,29 @@
 const express = require("express");
 const q2m = require("query-to-mongo");
 const ListingModel = require("./Schema");
+const TenantModel = require("../Tenants/Schema")
 const houseRoute = express.Router();
 const multer = require("multer");
 const path = require("path");
-
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const fs = require("fs-extra");
 const upload = multer({});
 const imagePath = path.join(__dirname, "../../public/images/homes");
 console.log(imagePath);
 
-const { adminOnlyMiddleware, authorize } = require("../middlewares/authorize");
+const {
+  adminOnlyMiddleware,
+  authorize
+} = require("../middlewares/authorize");
 
 houseRoute.get("/", async (req, res, next) => {
   try {
     const parsedQuery = q2m(req.query);
     const allListings = await ListingModel.find(
-      parsedQuery.criteria,
-      parsedQuery.options.fields
-    )
+        parsedQuery.criteria,
+        parsedQuery.options.fields
+      )
       .sort(parsedQuery.options.sort)
       .limit(parsedQuery.options.limit)
       .skip(parsedQuery.options.skip);
@@ -45,32 +50,32 @@ houseRoute.get("/:id", async (req, res, next) => {
   }
 });
 
-houseRoute.post(
-  "/:id/upload",
-  authorize,
-  upload.single("house"),
-  async (req, res, next) => {
-    try {
-      await fs.writeFile(
-        path.join(imagePath, `${req.params.id}.png`),
-        req.file.buffer
-      );
-      req.body = {
-        image: `http://localhost:5000/${req.params.id}.png`,
-      };
+// houseRoute.post(
+//   "/:id/upload",
+//   authorize,
+//   upload.single("house"),
+//   async (req, res, next) => {
+//     try {
+//       await fs.writeFile(
+//         path.join(imagePath, `${req.params.id}.png`),
+//         req.file.buffer
+//       );
+//       req.body = {
+//         image: `http://localhost:5000/${req.params.id}.png`,
+//       };
 
-      const post = await ListingModel.findByIdAndUpdate(
-        req.params.id,
-        req.body
-      );
-      if (post) {
-        res.send("image uploaded");
-      }
-    } catch (error) {
-      next(error);
-    }
-  }
-);
+//       const post = await ListingModel.findByIdAndUpdate(
+//         req.params.id,
+//         req.body
+//       );
+//       if (post) {
+//         res.send("image uploaded");
+//       }
+//     } catch (error) {
+//       next(error);
+//     }
+//   }
+// );
 houseRoute.post(
   "/uploadMultiple/:id",
   upload.array("houses"),
@@ -98,6 +103,85 @@ houseRoute.post(
     }
   }
 );
+houseRoute.post("/:id/tenants", async (req, res, next) => {
+
+  try {
+
+    const newTenant = new TenantModel({
+      ...req.body,
+      property: req.params.id
+    });
+    const savedTenant = await newTenant.save();
+    //res.status(201).send(newTenant);
+    console.log(savedTenant)
+    if (savedTenant) {
+      const list = await ListingModel.findById(req.params.id)
+      console.log(list)
+      console.log(list._id)
+      console.log(savedTenant.property)
+      const verfiedId = String(list._id) === String(savedTenant.property)
+      console.log(verfiedId)
+      if (verfiedId) {
+        console.log(list.description)
+        const msg = {
+          to: savedTenant.email, // Change to your recipient
+          from: "bridgehomes.realestate@gmail.com", // Change to your verified sender
+          subject: "Housing Application of " + list.title,
+          text: `${list.title}`,
+          html: `<h1>${list.title}</h1>
+                <strong>
+          
+                Description: ${list.description}
+          </strong>
+          <br>
+          
+           
+           <h4>District: ${list.district}</h4>
+            
+            <h4>Region: ${list.region}</h4>
+             
+             <h4>Price: ₦ ${list.price}</h4>
+              
+            <h4>Category: ${list.category}</h4>
+             
+             <h4>Address: ${list.location.formattedAddress}</h4>
+             
+             <h2>Thank You for contacting Bridge Homes</h2>
+             
+             <h5>Kind regards,<h5>
+             <br>
+             <h5>Abiodun Olarenwaju</h5>
+             <br>
+             <h5>CEO BRIDGE HOMES, Lagos Nigeria</h5>
+          `,
+        };
+        sgMail
+          .send(msg)
+          .then(() => {
+            console.log("Email sent");
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+        res.send("email Sent")
+
+      } else {
+        res.send("check conditios")
+      }
+
+
+    } else {
+      const error = new Error(`Experience with id ${req.params.id} not found`);
+      error.httpStatusCode = 404;
+      next(error);
+    }
+
+  } catch (error) {
+    next(error);
+  }
+})
+
+
 houseRoute.post("/image/:id", upload.array("post"), async (req, res, next) => {
   try {
     console.log(req.files);
@@ -113,25 +197,22 @@ houseRoute.post("/image/:id", upload.array("post"), async (req, res, next) => {
         );
         images.push(
           process.env.SERVER_URL +
-            process.env.PORT +
-            "/images/" +
-            req.params.id +
-            e.originalname
+          process.env.PORT +
+          "/images/" +
+          req.params.id +
+          e.originalname
         );
       })
     );
     await Promise.all(
       images.map(async (e) => {
-        const post = await ListingModel.update(
-          {
-            _id: req.params.id,
+        const post = await ListingModel.update({
+          _id: req.params.id,
+        }, {
+          $push: {
+            images: e,
           },
-          {
-            $push: {
-              images: e,
-            },
-          }
-        );
+        });
       })
     );
     const added = await ListingModel.findById(req.params.id);
@@ -183,7 +264,7 @@ houseRoute.get("/price/range", async (req, res, next) => {
     next(error);
   }
 });
-houseRoute.post("/", authorize, async (req, res, next) => {
+houseRoute.post("/", async (req, res, next) => {
   try {
     const newListing = new ListingModel(req.body);
     const addhouse = await newListing.save();
